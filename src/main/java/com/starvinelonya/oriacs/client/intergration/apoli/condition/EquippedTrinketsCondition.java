be01afd95cc295320d8551c411053e6b49c447d6
StarvineLonya <2272926227@qq.com>
2025-01-17 10:44:04 +0800

    items finished!

---
+++ src/main/java/com/starvinelonya/oriacs/client/intergration/apoli/condition/EquippedTrinketsCondition.java
@@ -0,0 +1,13 @@
+package com.starvinelonya.oriacs.client.intergration.apoli.condition;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
+import net.minecraft.client.render.model.json.ModelOverride;
+import net.minecraft.util.Identifier;
+
+import java.lang.module.Configuration;
+
+public class EquippedTrinketsCondition{
+
+}
